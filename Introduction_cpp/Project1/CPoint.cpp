72be019ad69ddfedef47dcec3c9fe13fb60c3d6a
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-10-05 15:56:23 +0200

    cout et cin ainsi que le namespace std dans iostream

--- Introduction_cpp/Project1/CPoint.cpp
+++ Introduction_cpp/Project1/CPoint.cpp
@@ -1,5 +1,4 @@
 #include "CPoint.h"
-#include <stdio.h>
 
 //Les constructeurs
 CPoint::CPoint()
@@ -12,14 +11,6 @@ CPoint::CPoint(int nX, int nY)
 {
 	setnX(nX);
 	setnY(nY);
-}
-
-void CPoint::affichePoint() const
-//BUT : Afficher les coordonnées d'un point dans la console.
-//ENTREE : Le-dit point.
-//SORTIE : Ses coordonnées X et Y affichées.
-{
-	printf("Mon point a pour coordonnees : X = %d Y = %d \n", getnX(), getnY());
 }
 
 //Accesseurs
--- Introduction_cpp/Project1/Main.cpp
+++ Introduction_cpp/Project1/Main.cpp
@@ -1,37 +1,24 @@
 #include "CPoint.h"
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	/*
-	printf("Hello World\n");
-
-	tPoint pPoint;
-
-	pPoint.m_nX = 4;
-	pPoint.m_nY = 1;
-
-	//cin >> pPoint.nX; //cin est l'équivalent de scanf en C++, cependant il récupère une valeur non typée.
-	//cin >> pPoint.nY;
-
-
-	pPoint.affichePoint(pPoint); //Note : Penser aux normes de programmations MVC.*/
-
 	//Création
-	CPoint pPoint(54,229);
-	CPoint pPoint2;
+	CPoint pPoint;
+	int nZ = 0;
 
-	//Initialisation.
-	/*pPoint.setnX(17);
-	pPoint.setnY(34);*/
+	cout << "Veuillez entrer la coordonnee en X du point." << endl;
+	
+	cin >> nZ;
+	pPoint.setnX(nZ);
 
-	//Appel de la méthode publique affichePoint()
-	//pPoint.affichePoint();
+	cout << "Veuillez entrer la coordonnee en Y du point." << endl;
 
-	printf("Mon point 1 a pour coordonnees : X = %d Y = %d \n", pPoint.getnX(), pPoint.getnY());
-	printf("Mon point 2 a pour coordonnees : X = %d Y = %d \n", pPoint2.getnX(), pPoint2.getnY());
+	cin >> nZ;
+	pPoint.setnY(nZ);
 
+	cout << "Notre point a pour coordonnees : X = " << pPoint.getnX() << " Y = " << pPoint.getnY() << endl;
 
 	return 0;
 }
