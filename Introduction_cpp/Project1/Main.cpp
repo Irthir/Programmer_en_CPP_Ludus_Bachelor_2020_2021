46262fb1c0c25f7b85e5363bb7435eb9c704878b
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-10-06 10:37:39 +0200

    Tentative pour la fonction échange point.

--- Introduction_cpp/Project1/Main.cpp
+++ Introduction_cpp/Project1/Main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+void EchangePoints(CPoint* pPoint, CPoint* pPoint1);
+
 int main()
 {
 	//Création
@@ -26,5 +28,27 @@ int main()
 
 	cout << pPoint.confondPoint(pPoint) << endl;
 
+	cout << "Mon point 1 a pour position : X = " << pPoint.getnX() << " Y = " << pPoint.getnY() << " et pour type : " << pPoint.getstrType() << "." << endl;
+	cout << "Mon point 2 a pour position : X = " << pPoing.getnX() << " Y = " << pPoing.getnY() << " et pour type : " << pPoing.getstrType() << "." << endl;
+	EchangePoints(&pPoint, &pPoing);
+	cout << "Mon point 1 a pour position : X = " << pPoint.getnX() << " Y = " << pPoint.getnY() << " et pour type : " << pPoint.getstrType() << "." << endl;
+	cout << "Mon point 2 a pour position : X = " << pPoing.getnX() << " Y = " << pPoing.getnY() << " et pour type : " << pPoing.getstrType() << "." << endl;
+
 	return 0;
+}
+
+void EchangePoints(CPoint *pPoint, CPoint *pPoint1)
+//BUT : Echanger la position de deux points.
+//ENTREE : Les pointeurs de ces deux points.
+//SORTIE : La position de ces deux points échangés.
+{
+	int nX = 0, nY = 0;
+	nX=pPoint->getnX();
+	nY=pPoint->getnY();
+	
+	pPoint->setnX(pPoint1->getnX());
+	pPoint->setnY(pPoint1->getnY());
+	
+	pPoint1->setnX(nX);
+	pPoint1->setnY(nY);
 }
